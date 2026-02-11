347efe406c23880e8b35c3e9dc9d51867a49995e
Joshua Neuwirth <CaptainBlake@users.noreply.github.com>
2026-02-11 22:54:07 +0100

    add Collector.py for data collection from Steam profiles

---
+++ Collector.py
@@ -0,0 +1,2 @@
+# will be used to collect data from the steam profiles. 
+# TODO: getting stuff from the steam profile: https://steamcommunity.com/id/<playerprofile>/gcpd/730/?tab=matchhistoryscrimmage
